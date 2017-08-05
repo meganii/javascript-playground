38caf75efd0075f24b98c3ca9c82957f74464fc5
meganii <yuhei24@gmail.com>
2017-08-05 09:20:48 +0900

    Add stock indicator

--- react-tutorial/my-app/src/Stock.js
+++ react-tutorial/my-app/src/Stock.js
@@ -5,10 +5,41 @@ import TrendingUp from 'material-ui/svg-icons/action/trending-up';
 import TrendingDown from 'material-ui/svg-icons/action/trending-down';
 import {blue500, red500, greenA200} from 'material-ui/styles/colors';
 
+import StockIndicator from './StockIndicator';
+
 class Stock extends Component {
+
+  state = {
+    data: []
+  };
+
+  componentWillMount() {
+    this.loadData();
+  }
+  loadData() {
+    fetch('./'+ this.props.code + '.json')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json);
+        this.setState({
+          data: json,
+        });
+        console.log(this.state.data.dataset.name);
+      });
+  }
+
   render() {
+    if (this.state.data.length == 0) {
+      return null;
+    };
+
+    const dataset = this.state.data.dataset;
+    const currentPrice = dataset.data[0][4];
+    const beforePrice = dataset.data[1][4];
+
     return (
       <ListItem 
+        key={this.props.index}
         className="Stock"
         primaryText={this.props.name}
         secondaryText={this.props.code + ':' + this.props.price}
@@ -16,10 +47,12 @@ class Stock extends Component {
         leftIcon={<div className="rightIcon">A</div>}
         rightAvatar={
           <div className="rightAvator">
-            <TrendingUp color={red500} />
-            <div>+{this.props.price}</div>
-            <div>(700)</div>
-          </div>}
+            <StockIndicator
+              currentPrice={currentPrice}
+              beforePrice={beforePrice}
+            />
+          </div>
+        }
       >
       </ListItem>
     );
--- react-tutorial/my-app/src/StockList.js
+++ react-tutorial/my-app/src/StockList.js
@@ -9,14 +9,14 @@ class StockList extends Component {
   render() {
     const data = {
       stocks: [
-        {name: 'とーそー', price: 100, code: '2456T'},
-        {name: '伊藤園', price: 100, code: '2345T'},
-        {name: 'あんしん保証', price: 300, code: '1124T'},
+        {name: 'トーソー', price: 100, code: '5956', avgBuyPrice: 400, numberOfSharesHeld: 100},
+        {name: '伊藤園', price: 100, code: '2593', avgBuyPrice: 5000, numberOfSharesHeld: 100},
+        {name: 'あんしん保証', price: 300, code: '7183', avgBuyPrice: 550, numberOfSharesHeld: 100},
       ]
     };
-    const stockList = data.stocks.map((stock) =>
+    const stockList = data.stocks.map((stock,index) =>
       <div className="StockItem">
-        <Stock name={stock.name} price={stock.price} code={stock.code}/>
+        <Stock index={index} name={stock.name} price={stock.price} code={stock.code}/>
         <Divider />
       </div>
     );
